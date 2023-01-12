ab37e926ac8b7d4aa01d693c6d8aab1dc665a2e9
Jon Seager <jon@sgrs.uk>
2023-01-12 09:40:41 +0000

    feat: add thermald/throttled for odin

--- hosts/odin/default.nix
+++ hosts/odin/default.nix
@@ -1,14 +1,20 @@
 { pkgs, lib, ... }: {
-  imports = [ 
+  imports = [
     ./hardware.nix
     ./optional/desktop.nix
     ../common/global
     ../common/users/jon
   ];
 
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv6.conf.all.forwarding" = 1;
+  boot = {
+    kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "net.ipv6.conf.all.forwarding" = 1;
+    };
+    # Force S3 sleep mode. See README.wiki for details.
+    kernelParams = [ "mem_sleep_default=deep" ];
+    # XPS 9370 touchpad goes over i2c
+    blacklistedKernelModules = [ "psmouse" ];
   };
 
   networking = {
@@ -17,14 +23,17 @@
     hostId = "17d07435";
     useDHCP = lib.mkDefault true;
   };
-  
+
+  # Power, throttling, etc.
   services.upower.enable = true;
+  services.throttled.enable = lib.mkDefault true;
+  services.thermald.enable = true;
 
   virtualisation = {
     containerd.enable = true;
     docker = {
-        enable = true;
-        storageDriver = "btrfs";
+      enable = true;
+      storageDriver = "btrfs";
     };
   };
   environment.systemPackages = [ pkgs.ctop ];
