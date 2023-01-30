fca83060c187563a7500297b6564040acb68c278
Jon Seager <jon@sgrs.uk>
2023-01-30 16:14:21 +0000

    conf: add display configs for loki

--- home/common/optional/sway/config/displays.nix
+++ home/common/optional/sway/config/displays.nix
@@ -28,7 +28,22 @@
     ];
 
     kanshi-profiles = {
-      default = {};
+      default = {
+        outputs = [
+          {
+            status = "enable";
+            criteria = "HDMI-A-1";
+            position = "3840,0";
+            mode = "3840x2160@60.000Hz";
+          }
+          {
+            status = "enable";
+            criteria = "HDMI-A-2";
+            position = "0,0";
+            mode = "3840x2160@60.000Hz";
+          }
+        ];
+      };
     };
   };
 
