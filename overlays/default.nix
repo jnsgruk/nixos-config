54b510b27cf2a56483d9098ffb92713d3698dc42
Jon Seager <jon@sgrs.uk>
2023-02-20 19:59:59 +0000

    fix: override catppuccin-nvim plugin to use older revision

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,15 +4,30 @@
 
   modifications = _final: prev: {
     # Get latest commit from swaylock-effects to fix Sway 1.8 issue
-    swaylock-effects = prev.swaylock-effects.overrideAttrs (_old: {
-      version = "1.6.10+cd07dd10";
-      src = prev.fetchFromGitHub {
-        owner = "jirutka";
-        repo = "swaylock-effects";
-        rev = "cd07dd1082a2fc1093f1e6f2541811e446f4d114";
-        hash = "sha256-aK/PvFjZoF8R0llXO+P650vHYLSoGS6dYSk5Pw8DBNY=";
+    # swaylock-effects = prev.swaylock-effects.overrideAttrs (_old: {
+    #   version = "1.6.10+cd07dd10";
+    #   src = prev.fetchFromGitHub {
+    #     owner = "jirutka";
+    #     repo = "swaylock-effects";
+    #     rev = "cd07dd1082a2fc1093f1e6f2541811e446f4d114";
+    #     hash = "sha256-aK/PvFjZoF8R0llXO+P650vHYLSoGS6dYSk5Pw8DBNY=";
+    #   };
+    # });
+
+    vimPlugins = prev.vimPlugins // {
+      catppuccin-nvim = prev.vimUtils.buildVimPluginFrom2Nix {
+        pname = "catppuccin-nvim";
+        version = "2023-02-12";
+
+        src = prev.fetchFromGitHub {
+          owner = "catppuccin";
+          repo = "nvim";
+          rev = "a5f3ed5d3b1d9ea21183718a8a89a6653bd6ea48";
+          sha256 = "1vx7p3f8339v1w9ww9l1lg3s6wf699q2bp762aqkmwmh88ykhi8i";
+
+        };
       };
-    });
+    };
 
     # Augment the tmuxPlugins package with an additional 'catppuccin' theme plugin
     tmuxPlugins =
@@ -32,5 +47,6 @@
           '';
         };
       };
+
   };
 }
