469aca1a02eae95f2d27b37ca812a80db57ce9ca
Jon Seager <jon@sgrs.uk>
2023-02-21 16:12:59 +0000

    feat: use rambox from nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,5 @@
 { pkgs }: {
   catppuccin-macchiato-gtk = pkgs.callPackage ./catppuccin-macchiato-gtk.nix { };
-  rambox = pkgs.callPackage ./rambox.nix { };
   sfpro-font = pkgs.callPackage ./sfpro-font.nix { };
   sf-mono-liga-font = pkgs.callPackage ./sf-mono-liga-font.nix { };
   sway-scripts = pkgs.callPackage ./sway-scripts { };
