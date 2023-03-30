4f702febbc776ca89b3d6f0415da609c99229dda
Jon Seager <jon@sgrs.uk>
2023-03-30 12:27:30 +0200

    feat: add wdisplays and libva-utils

--- home/common/optional/sway/packages.nix
+++ home/common/optional/sway/packages.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
     grim
     slurp
     swappy
+    libva-utils
     wf-recorder
     wl-clipboard
+    wdisplays
     wmctrl
   ];
 }
