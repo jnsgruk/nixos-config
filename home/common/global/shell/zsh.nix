28026adcc85317559416770852476b43112cc15f
Jon Seager <jon@sgrs.uk>
2023-04-13 11:18:37 +0100

    conf: simplify zsh config

--- home/common/global/shell/zsh.nix
+++ home/common/global/shell/zsh.nix
@@ -15,28 +15,14 @@
       };
 
       initExtra = ''
-        bindkey -e
-        bindkey  "^[[H"   beginning-of-line
-        bindkey  "^[[F"   end-of-line
-        bindkey  "^[[3~"  delete-char
-        bindkey '^[[1;9C' forward-word # Ctrl+RightArrow
-        bindkey '^[[1;9D' backward-word # Ctrl+LeftArrow
-        bindkey "\033[1~" beginning-of-line
-        bindkey "\033[4~" end-of-line
-
-        zstyle :compinstall filename $HOME/.zshrc
-        autoload -U colors && colors
-        autoload -Uz compinit && compinit
-        autoload -U +X bashcompinit && bashcompinit
+        bindkey '^[[1;5C' forward-word # Ctrl+RightArrow
+        bindkey '^[[1;5D' backward-word # Ctrl+LeftArrow
 
         zstyle ':completion:*' completer _complete _match _approximate
         zstyle ':completion:*:match:*' original only
         zstyle ':completion:*:approximate:*' max-errors 1 numeric
         zstyle ':completion:*' menu select
         zstyle ':completion:*' list-colors "''${(s.:.)LS_COLORS}"
-
-        autoload -U promptinit
-        promptinit
 
         # HACK! Simple shell function to patch ruff bins downloaded by tox from PyPI to use
         # the ruff included in NixOS - needs to be run each time the tox enviroment is
