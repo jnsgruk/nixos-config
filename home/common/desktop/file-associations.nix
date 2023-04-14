9254e0cc85f19ec8872706fe8ba4970a5d4ae377
Jon Seager <jon@sgrs.uk>
2023-04-14 15:54:05 +0100

    refactor: split out mime-type file associations

---
+++ home/common/desktop/file-associations.nix
@@ -0,0 +1,54 @@
+{
+
+  browser = [ "google-chrome.desktop" ];
+  archiveManager = [ "org.gnome.FileRoller.desktop" ];
+
+  # XDG MIME types
+  associations = {
+    "application/x-extension-htm" = browser;
+    "application/x-extension-html" = browser;
+    "application/x-extension-shtml" = browser;
+    "application/x-extension-xht" = browser;
+    "application/x-extension-xhtml" = browser;
+    "application/xhtml+xml" = browser;
+    "text/html" = browser;
+    "x-scheme-handler/about" = browser;
+    "x-scheme-handler/chrome" = browser;
+    "x-scheme-handler/ftp" = browser;
+    "x-scheme-handler/http" = browser;
+    "x-scheme-handler/https" = browser;
+    "x-scheme-handler/unknown" = browser;
+
+    "audio/*" = [ "mpv.desktop" ];
+    "video/*" = [ "mpv.dekstop" ];
+    "image/*" = [ "feh.desktop" ];
+
+    "application/json" = browser;
+
+    "application/pdf" = [ "org.pwmt.zathura.desktop.desktop" ];
+
+    # Archives / compressed files
+    "application/x-7z-compressed" = archiveManager;
+    "application/x-7z-compressed-tar" = archiveManager;
+    "application/x-bzip" = archiveManager;
+    "application/x-bzip-compressed-tar" = archiveManager;
+    "application/x-compress" = archiveManager;
+    "application/x-compressed-tar" = archiveManager;
+    "application/x-cpio" = archiveManager;
+    "application/x-gzip" = archiveManager;
+    "application/x-lha" = archiveManager;
+    "application/x-lzip" = archiveManager;
+    "application/x-lzip-compressed-tar" = archiveManager;
+    "application/x-lzma" = archiveManager;
+    "application/x-lzma-compressed-tar" = archiveManager;
+    "application/x-tar" = archiveManager;
+    "application/x-tarz" = archiveManager;
+    "application/x-xar" = archiveManager;
+    "application/x-xz" = archiveManager;
+    "application/x-xz-compressed-tar" = archiveManager;
+    "application/zip" = archiveManager;
+    "application/gzip" = archiveManager;
+    "application/bzip2" = archiveManager;
+    "application/vnd.rar" = archiveManager;
+  };
+}
--- home/common/desktop/xdg.nix
+++ home/common/desktop/xdg.nix
@@ -1,29 +1,6 @@
 { ... }:
 let
-  browser = [ "google-chrome.desktop" ];
-
-  # XDG MIME types
-  associations = {
-    "application/x-extension-htm" = browser;
-    "application/x-extension-html" = browser;
-    "application/x-extension-shtml" = browser;
-    "application/x-extension-xht" = browser;
-    "application/x-extension-xhtml" = browser;
-    "application/xhtml+xml" = browser;
-    "text/html" = browser;
-    "x-scheme-handler/about" = browser;
-    "x-scheme-handler/chrome" = browser;
-    "x-scheme-handler/ftp" = browser;
-    "x-scheme-handler/http" = browser;
-    "x-scheme-handler/https" = browser;
-    "x-scheme-handler/unknown" = browser;
-
-    "audio/*" = [ "mpv.desktop" ];
-    "video/*" = [ "mpv.dekstop" ];
-    "image/*" = [ "feh.desktop" ];
-    "application/json" = browser;
-    "application/pdf" = [ "org.pwmt.zathura.desktop.desktop" ];
-  };
+  associations = (import ./file-associations.nix).associations;
 in
 {
   xdg = {
