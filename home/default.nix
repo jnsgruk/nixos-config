936164b880e819c8fa959f2c4668bd08f9e87b7b
Jon Seager <jon@sgrs.uk>
2023-04-17 21:27:45 +0100

    chore: update flake lock file

--- home/default.nix
+++ home/default.nix
@@ -35,7 +35,7 @@
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = _: true;
       permittedInsecurePackages = [
-        "electron-18.1.0"
+        "electron-21.4.0"
       ];
     };
   };
