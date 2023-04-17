89fd8765dcaeb83791b6055e0dd666553994db68
Jon Seager <jon@sgrs.uk>
2023-04-17 11:28:22 +0100

    feat: include quickemu on desktop hosts

--- host/common/virt/default.nix
+++ host/common/virt/default.nix
@@ -1,8 +1,9 @@
-{ ... }: {
+{ desktop, lib, ... }: {
   imports = [
     ./embr.nix
     ./docker.nix
     ./lxd
     ./multipass.nix
-  ];
+  ] # Include quickemu if a desktop is defined
+  ++ lib.optional (builtins.isString desktop) ./quickemu.nix;
 }
