57a04a6480153776d9e0d544a03a04019ee34064
Jon Seager <jon@sgrs.uk>
2023-04-19 17:58:51 +0100

    feat: enable telescope, vim-visual-multi, devicons, nvim-tree

--- home/common/shell/vim.nix
+++ home/common/shell/vim.nix
@@ -21,6 +21,8 @@
       vim.opt.tabstop = 2
 
       vim.opt.backspace = "indent,eol,start"
+      vim.g.mapleader = ';'
+      vim.g.maplocalleader = ';'
 
       vim.opt.hlsearch = true
       vim.opt.ignorecase = true
@@ -29,10 +31,39 @@
       vim.opt.number = true
       vim.opt.background = dark
       vim.opt.termguicolors = true
+
+      function map(mode, lhs, rhs, opts)
+        local options = {noremap = true, silent = true}
+        if opts then
+          options = vim.tbl_extend("force", options, opts)
+        end
+        vim.keymap.set(mode, lhs, rhs, options)
+      end
+
+      map('n', '<leader>t',        ':ToggleTerm<CR>'      )
+      map('i', '<leader><leader>', '<Esc>'                )
+      map('t', '<Esc>',            '<C-\\><C-n>'          )
+      map('n', '<leader>p',        ':NvimTreeToggle<CR>'  )
+      map('n', '<leader><Space>',  ':NvimTreeToggle<CR>'  )
+      map('n', '<leader>ff',       ':Telescope find_files<CR>')
+      map('n', '<leader>fg',       ':Telescope live_grep<CR>' )
+      map('n', '<leader>fb',       ':Telescope buffers<CR>'   )
+      map('n', '<leader>fh',       ':Telescope help_tags<CR>' )
+      map('n', '<leader>fo',       ':Telescope vim_options<CR>' )
     '';
 
     plugins = with pkgs.vimPlugins; [
+      nvim-web-devicons
+      telescope-nvim
       vim-nix
+      vim-visual-multi
+      {
+        plugin = nvim-tree-lua;
+        type = "lua";
+        config = ''
+          require('nvim-tree').setup({ update_cwd = true })
+        '';
+      }
       {
         plugin = lightline-vim;
         type = "viml";
