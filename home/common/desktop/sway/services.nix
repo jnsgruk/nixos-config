97013e553890a0d7b8779d10468d02e6a798d2b4
Jon Seager <jon@sgrs.uk>
2023-04-20 13:20:54 +0100

    fix: move the polkit-gnome-authentication-agent service to home-manager

--- home/common/desktop/sway/services.nix
+++ home/common/desktop/sway/services.nix
@@ -41,4 +41,16 @@
       longitude = "-2.53";
     };
   };
+
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    Unit.Description = "polkit-gnome-authentication-agent-1";
+    Install.WantedBy = [ "graphical-session.target" ];
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
 }
--- host/common/desktop/sway.nix
+++ host/common/desktop/sway.nix
@@ -39,23 +39,6 @@
     gvfs.enable = true;
   };
 
-  systemd = {
-    user.services.polkit-gnome-authentication-agent-1 = {
-      unitConfig = {
-        Description = "polkit-gnome-authentication-agent-1";
-        Wants = [ "graphical-session.target" ];
-        After = [ "graphical-session.target" ];
-      };
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
-
   security = {
     pam = {
       services = {
