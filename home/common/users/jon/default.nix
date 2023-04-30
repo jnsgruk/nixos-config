fb8b383fa5cf466e1b7f3895334330964b1a276a
Jon Seager <jon@sgrs.uk>
2023-04-30 11:16:10 +0200

    style: simplify per host imports in home-manager

--- home/common/users/jon/default.nix
+++ home/common/users/jon/default.nix
@@ -1,10 +1,4 @@
-{ lib, hostname, ... }:
-let
-  # If there is a ./${hostname}.nix then add it to a list of imports.
-  # Allows for per-user, per-host customisation where required.
-  extraHostConfig = (./. + "/${hostname}.nix");
-  extraHostImport = if (builtins.pathExists extraHostConfig) then [ extraHostConfig ] else [ ];
-in
-{
-  imports = [ ] ++ extraHostImport;
+{ lib, hostname, ... }: {
+  imports = [ ]
+    ++ lib.optional (builtins.pathExists (./. + "/${hostname}.nix")) ./${hostname}.nix;
 }
