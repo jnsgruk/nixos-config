a667ff047c203349c196f68ab15b284aa5c2e73a
Jon Seager <jon@sgrs.uk>
2023-05-09 12:52:58 +0100

    conf: make swappy save screenshots in a subdir of $XDG_PICTURES_DIR

--- home/common/desktop/sway/packages.nix
+++ home/common/desktop/sway/packages.nix
@@ -13,7 +13,7 @@
   home.file = {
     ".config/swappy/config".text = ''
       [Default]
-      save_dir=$HOME/pictures
+      save_dir=$HOME/pictures/screenshots
       save_filename_format=screenshot-%Y%m%d-%H%M%S.png
       early_exit=true
     '';
