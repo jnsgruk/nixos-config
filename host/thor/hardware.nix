4ed88206ad6eab4a71c3bb87ae7dc5505c30315a
Jon Seager <jon@sgrs.uk>
2023-05-10 13:54:46 +0100

    fix: remove redundant create-swapfile service definition

--- host/thor/hardware.nix
+++ host/thor/hardware.nix
@@ -54,23 +54,4 @@
     device = "/.swap/swapfile";
     size = 2048;
   }];
-
-  systemd.services = {
-    create-swapfile = {
-      serviceConfig.type = "oneshot";
-      wantedBy = [ "swap-swapfile.swap" ];
-      script = ''
-        swapdir="/.swap"
-        swapfile="$swapdir/swapfile"
-        if [[ -f "$swapfile" ]]; then
-          echo "Swapfile $swapfile already exists"
-        else
-          ${pkgs.coreutils}/bin/mkdir "$swapdir"
-          ${pkgs.coreutils}/bin/truncate -s 0 "$swapfile"
-          ${pkgs.e2fsprogs}/bin/chattr +C "$swapfile"
-          ${pkgs.btrfs-progs}/bin/btrfs property set "$swapfile" compression none
-        fi
-      '';
-    };
-  };
 }
