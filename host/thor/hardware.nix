03864033608a85d267cab10607570524c9aa3aab
Jon Seager <jon@sgrs.uk>
2023-05-11 17:03:06 +0100

    feat: initial disko config for thor

--- host/thor/hardware.nix
+++ host/thor/hardware.nix
@@ -3,10 +3,13 @@
 , ...
 }: {
   imports = [
+    inputs.disko.nixosModules.disko
     inputs.nixos-hardware.nixosModules.common-cpu-intel
     inputs.nixos-hardware.nixosModules.common-hidpi
     inputs.nixos-hardware.nixosModules.common-pc
     inputs.nixos-hardware.nixosModules.common-pc-ssd
+
+    (import ./disks.nix { })
   ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
@@ -15,44 +18,20 @@
     enableAllFirmware = true;
   };
 
-  # TODO: Replace with Disko config
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/67e2aec0-abb8-4d0d-b8d4-556a8b2f3532";
-    fsType = "btrfs";
-    options = [ "subvol=@" "compress=lzo" "noatime" "nodiratime" "ssd" ];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/67e2aec0-abb8-4d0d-b8d4-556a8b2f3532";
-    fsType = "btrfs";
-    options = [ "subvol=@home" "compress=lzo" "noatime" "nodiratime" "ssd" ];
-  };
-
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/67e2aec0-abb8-4d0d-b8d4-556a8b2f3532";
-    fsType = "btrfs";
-    options = [ "subvol=@var" "compress=lzo" "noatime" "nodiratime" "ssd" ];
-  };
-
-  fileSystems."/.snapshots" = {
-    device = "/dev/disk/by-uuid/67e2aec0-abb8-4d0d-b8d4-556a8b2f3532";
-    fsType = "btrfs";
-    options = [ "subvol=@snapshots" "compress=lzo" "noatime" "nodiratime" "ssd" ];
-  };
-
-  fileSystems."/.swap" = {
-    device = "/dev/disk/by-uuid/67e2aec0-abb8-4d0d-b8d4-556a8b2f3532";
-    fsType = "btrfs";
-    options = [ "subvol=@swap" ];
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/FD29-48A2";
-    fsType = "vfat";
-  };
-
   swapDevices = [{
     device = "/.swap/swapfile";
     size = 2048;
   }];
+
+  # Setup additional 1TB data drive with LUKS encryption
+  # fileSystems."/data" = {
+  #   device = "/dev/mapper/data";
+  #   fsType = "ext4";
+  # };
+
+  environment.etc = {
+    "crypttab".text = ''
+      data  /dev/disk/by-partlabel/data  /etc/data.keyfile
+    '';
+  };
 }
