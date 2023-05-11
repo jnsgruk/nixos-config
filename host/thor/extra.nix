761a520ec6391236dc9831167896965ab2a801f3
Jon Seager <jon@sgrs.uk>
2023-05-11 21:59:27 +0100

    conf: add nfs config to thor

--- host/thor/extra.nix
+++ host/thor/extra.nix
@@ -2,4 +2,19 @@
   imports = [
     ../common/virt/docker.nix
   ];
+
+  # Firewall port for NFS
+  networking.firewall.allowedTCPPorts = [ 2049 ];
+
+  # Setup NFS exports
+  services = {
+    nfs = {
+      server = {
+        enable = true;
+        exports = ''
+          /data/media  100.64.0.0/10 (rw,fsid=0,no_subtree_check)
+        '';
+      };
+    };
+  };
 }
