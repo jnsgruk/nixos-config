ac57376633b3bafcd93e470196c498a8e5def3c0
Jon Seager <jon@sgrs.uk>
2023-05-16 20:57:06 +0100

    conf: update sway session variables for wayland support

--- home/common/desktop/sway/default.nix
+++ home/common/desktop/sway/default.nix
@@ -74,11 +74,13 @@
 
   home.sessionVariables = {
     _JAVA_AWT_WM_NONREPARENTING = "1";
+    CLUTTER_BACKEND = "wayland";
+    GDK_BACKEND = "wayland";
     MOZ_ENABLE_WAYLAND = "1";
     QT_QPA_PLATFORM = "wayland";
+    # QT_QPA_PLATFORMTHEME = "qt5ct";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
     SDL_VIDEODRIVER = "wayland";
     XDG_SESSION_TYPE = "wayland";
-    GDK_BACKEND = "wayland";
   };
 }
