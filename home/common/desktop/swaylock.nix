079fc095a9bc8a45cc4a51d38ca45fd99e93f7c8
Jon Seager <jon@sgrs.uk>
2023-05-18 08:45:16 +0100

    conf: make swaylock config more explicit; name package

--- home/common/desktop/swaylock.nix
+++ home/common/desktop/swaylock.nix
@@ -1,47 +1,53 @@
 { lib, pkgs, desktop, ... }: {
-  programs.swaylock.settings = {
-    font = "SF Pro";
-    clock = true;
+  programs.swaylock = {
+    enable = true;
+    package = pkgs.swaylock-effects;
 
-    indicator = true;
-    ignore-empty-password = true;
-    indicator-idle-visible = true;
-    disable-caps-lock-text = true;
+    settings = {
 
-    timestr = "%R";
-    datestr = "%a, %e of %B";
-    image = "${./wallpapers/elk-colors.jpg}";
-    # effect-blur = "30x3";
+      font = "SF Pro";
+      clock = true;
 
-    key-hl-color = "880033";
-    separator-color = "00000000";
+      indicator = true;
+      ignore-empty-password = true;
+      indicator-idle-visible = true;
+      disable-caps-lock-text = true;
 
-    inside-color = "00000099";
-    inside-clear-color = "ffd20400";
-    inside-caps-lock-color = "009ddc00";
-    inside-ver-color = "d9d8d800";
-    inside-wrong-color = "ee2e2400";
+      timestr = "%R";
+      datestr = "%a, %e of %B";
+      image = "${./wallpapers/elk-colors.jpg}";
+      # effect-blur = "30x3";
 
-    ring-color = "231f20D9";
-    ring-clear-color = "231f20D9";
-    ring-caps-lock-color = "231f20D9";
-    ring-ver-color = "231f20D9";
-    ring-wrong-color = "231f20D9";
+      key-hl-color = "880033";
+      separator-color = "00000000";
 
-    line-color = "00000000";
-    line-clear-color = "ffd204FF";
-    line-caps-lock-color = "009ddcFF";
-    line-ver-color = "d9d8d8FF";
-    line-wrong-color = "ee2e24FF";
+      inside-color = "00000099";
+      inside-clear-color = "ffd20400";
+      inside-caps-lock-color = "009ddc00";
+      inside-ver-color = "d9d8d800";
+      inside-wrong-color = "ee2e2400";
 
-    text-clear-color = "ffd20400";
-    text-ver-color = "d9d8d800";
-    text-wrong-color = "ee2e2400";
+      ring-color = "231f20D9";
+      ring-clear-color = "231f20D9";
+      ring-caps-lock-color = "231f20D9";
+      ring-ver-color = "231f20D9";
+      ring-wrong-color = "231f20D9";
 
-    bs-hl-color = "ee2e24FF";
-    caps-lock-key-hl-color = "ffd204FF";
-    caps-lock-bs-hl-color = "ee2e24FF";
-    text-caps-lock-color = "009ddc";
+      line-color = "00000000";
+      line-clear-color = "ffd204FF";
+      line-caps-lock-color = "009ddcFF";
+      line-ver-color = "d9d8d8FF";
+      line-wrong-color = "ee2e24FF";
+
+      text-clear-color = "ffd20400";
+      text-ver-color = "d9d8d800";
+      text-wrong-color = "ee2e2400";
+
+      bs-hl-color = "ee2e24FF";
+      caps-lock-key-hl-color = "ffd204FF";
+      caps-lock-bs-hl-color = "ee2e24FF";
+      text-caps-lock-color = "009ddc";
+    };
   };
 
   services.swayidle = {
