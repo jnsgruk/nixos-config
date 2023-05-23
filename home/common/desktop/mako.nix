e388fefaff7f755ae66379da8e7a92682ab83a82
Jon Seager <jon@sgrs.uk>
2023-05-23 14:05:39 +0100

    conf: update mako notification style

--- home/common/desktop/mako.nix
+++ home/common/desktop/mako.nix
@@ -4,16 +4,16 @@
       enable = true;
       actions = true;
       anchor = "top-right";
-      borderRadius = 0;
+      borderRadius = 8;
       borderSize = 1;
       defaultTimeout = 10000;
       font = "SF Pro";
       iconPath = "${pkgs.papirus-icon-theme}/share/icons/Papirus-Dark";
       icons = true;
       layer = "overlay";
-      maxVisible = 5;
-      padding = "20";
-      width = 450;
+      maxVisible = 3;
+      padding = "10";
+      width = 300;
 
       backgroundColor = "#24273a";
       borderColor = "#8aadf4";
