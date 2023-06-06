6fb0dbb528623020bc9fe1c47451c35ec7c4e983
Jon Seager <jon@sgrs.uk>
2023-06-06 15:07:26 +0100

    conf: update syncthing addresses

--- home/common/users/jon/freyja.nix
+++ home/common/users/jon/freyja.nix
@@ -2,7 +2,7 @@
   services.syncthing = {
     enable = true;
     extraOptions = [
-      "-gui-address=100.125.106.101:8384"
+      "-gui-address=100.78.135.96:8384"
       "-home=/home/jon/data/.syncthing"
     ];
   };
