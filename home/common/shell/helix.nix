3896e68f0d541b86547730d1ad0b857a3e1f2482
Jon Seager <jon@sgrs.uk>
2023-06-13 12:56:37 +0100

    feat: seperate language server config for helix

--- home/common/shell/helix.nix
+++ home/common/shell/helix.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, lib, ... }: {
   programs.helix = {
     enable = true;
     settings = {
@@ -20,26 +20,24 @@
         rulers = [ 100 ];
       };
     };
-    languages = {
-      nix.auto-format = true;
-      go.auto-format = true;
-      python = {
-        auto-format = true;
-        formatter = {
-          command = "black";
-          args = [ "-l100" "--quiet" "-" ];
-        };
-        config = {
-          pylsp = {
-            plugins = {
-              ruff = {
-                enabled = true;
-                lineLength = 100;
-              };
-            };
-          };
-        };
-      };
-    };
   };
+
+  # The home-manager module doesn't seem to work right specifying LSP options
+  # at the moment, so reverting to setting the file up manually.
+  xdg.configFile."helix/languages.toml".text = ''
+    [[language]]
+    name = "nix"
+    auto-format = true
+    language-server = { command = "${lib.getExe pkgs.nil}" }
+    formatter = { command = "${lib.getExe pkgs.nixpkgs-fmt}" }
+
+    [[language]]
+    name = "python"
+    auto-format = true
+    formatter = { command = "black", args = ["-l99", "-q", "-"]}
+
+    [[language]]
+    name = "go"
+    auto-format = true
+  '';
 }
