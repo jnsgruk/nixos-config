4fc6a293643db16b9cbcdfdac6d12beb129bc396
Jon Seager <jon@sgrs.uk>
2023-06-22 19:19:13 +0100

    fix: update tailscale address for syncthing on kara

--- home/common/users/jon/kara.nix
+++ home/common/users/jon/kara.nix
@@ -2,7 +2,7 @@
   services.syncthing = {
     enable = true;
     extraOptions = [
-      "-gui-address=100.64.118.37:8384"
+      "-gui-address=100.93.165.28:8384"
       "-home=/home/jon/data/.syncthing"
     ];
   };
