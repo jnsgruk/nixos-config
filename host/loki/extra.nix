347c38816c240bff44d4ab709b26135530eb46e6
Jon Seager <jon@sgrs.uk>
2023-06-26 10:23:54 +0100

    feat: enable NFS media share on loki

--- host/loki/extra.nix
+++ host/loki/extra.nix
@@ -2,4 +2,22 @@
   imports = [
     ../common/virt
   ];
+
+  # Firewall port for NFS
+  networking.firewall = {
+    allowedTCPPorts = [ 111 2049 4000 4001 4002 20048 ];
+    allowedUDPPorts = [ 111 2049 4000 4001 4002 20048 ];
+  };
+
+  # Setup NFS exports
+  services = {
+    nfs = {
+      server = {
+        enable = true;
+        exports = ''
+          /data/media  100.64.0.0/10 (rw,fsid=0,no_subtree_check)
+        '';
+      };
+    };
+  };
 }
