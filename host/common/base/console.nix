ee5beb655a24c0fe4916d3ee2bdc64004ca7c93d
Jon Seager <jon@sgrs.uk>
2023-07-11 10:53:14 +0100

    feat: enable palette settings in kmscon

--- host/common/base/console.nix
+++ host/common/base/console.nix
@@ -1,7 +1,12 @@
 { pkgs
 , theme
 , ...
-}: {
+}:
+let
+  colours = theme.colours;
+  hexToRgb = theme.hexToRgb;
+in
+{
   console = {
     earlySetup = true;
     packages = with pkgs; [ terminus_font powerline-fonts ];
@@ -17,7 +22,28 @@
     }];
     extraConfig = ''
       font-size=14
+
+      xkb-layout=gb
+
+      palette=custom
+      palette-black=${hexToRgb colours.surface1}
+      palette-red=${hexToRgb colours.red}
+      palette-green=${hexToRgb colours.green}
+      palette-yellow=${hexToRgb colours.yellow}
+      palette-blue=${hexToRgb colours.darkBlue}
+      palette-magenta=${hexToRgb colours.pink}
+      palette-cyan=${hexToRgb colours.cyan}
+      palette-light-grey=${hexToRgb colours.overlay2}
+      palette-dark-grey=${hexToRgb colours.overlay0}
+      palette-light-red=${hexToRgb colours.red}
+      palette-light-green=${hexToRgb colours.green}
+      palette-light-yellow=${hexToRgb colours.yellow}
+      palette-light-blue=${hexToRgb colours.darkBlue}
+      palette-light-magenta=${hexToRgb colours.pink}
+      palette-light-cyan=${hexToRgb colours.cyan}
+      palette-white=${hexToRgb colours.subtext1}
+      palette-foreground=${hexToRgb colours.text}
+      palette-background=${hexToRgb colours.bg}
     '';
-    extraOptions = "--xkb-layout=gb";
   };
 }
