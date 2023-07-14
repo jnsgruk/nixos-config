7611a962b5261e5ed087595d6154e74d24f924ff
Jon Seager <jon@sgrs.uk>
2023-07-14 16:49:22 +0100

    feat: use zsh in flake shell

--- shell.nix
+++ shell.nix
@@ -2,8 +2,12 @@
 # Access development shell with  'nix develop' or (legacy) 'nix-shell'
 { pkgs ? (import ./nixpkgs.nix) { } }: {
   default = pkgs.mkShell {
+    name = "jnsgruk-flake";
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [ nix home-manager git ];
+    shellHook = ''
+      exec zsh
+    '';
   };
 }
