f6b896c4495fb1d763c06c1466c9c0fa596f9a7b
Jon Seager <jon@sgrs.uk>
2023-07-15 19:11:35 +0100

    conf: move state directory for homepage

--- host/common/services/homepage.nix
+++ host/common/services/homepage.nix
@@ -3,17 +3,17 @@
     allowedTCPPorts = [ 8082 ];
   };
 
-  systemd.services.homepage = {
+  systemd.services.homepage-dashboard = {
     enable = true;
     description = "A highly customizable homepage (or startpage / application dashboard) with Docker and service API integrations.";
     environment = {
-      HOMEPAGE_CONFIG_DIR = "/var/lib/homepage";
+      HOMEPAGE_CONFIG_DIR = "/var/lib/homepage-dashboard";
       PORT = "8082";
     };
     serviceConfig = {
       Type = "simple";
       DynamicUser = true;
-      StateDirectory = "homepage";
+      StateDirectory = "homepage-dashboard";
       ExecStart = "${pkgs.homepage}/bin/homepage";
       Restart = "on-failure";
     };
