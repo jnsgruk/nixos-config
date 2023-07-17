665a853ab7fe5fbd88d72bd83f60eddcb4d7260c
Jon Seager <jon@sgrs.uk>
2023-07-17 11:31:19 +0100

    fix: kara syncthing config bind address

--- home/common/users/jon/kara.nix
+++ home/common/users/jon/kara.nix
@@ -2,7 +2,7 @@ _: {
   services.syncthing = {
     enable = true;
     extraOptions = [
-      "-gui-address=freyja.tailnet-d5da.ts.net:8384"
+      "-gui-address=kara.tailnet-d5da.ts.net:8384"
       "-home=/home/jon/data/.syncthing"
     ];
   };
