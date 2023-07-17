b0e22920ce559fa60e68241e05477f203a7372be
Jon Seager <jon@sgrs.uk>
2023-07-17 11:02:50 +0100

    fix: update lxd package config

--- host/common/virt/lxd.nix
+++ host/common/virt/lxd.nix
@@ -6,14 +6,13 @@ in
   # Always use the lxd module from nixos-unstable
   disabledModules = [ "${inputs.nixpkgs}/${lxdModule}" ];
   imports = [ "${inputs.nixpkgs-unstable}/${lxdModule}" ];
-
   virtualisation = {
     lxd = {
       enable = true;
       zfsSupport = true;
       ui = {
         enable = true;
-        package = pkgs.unstable.lxd.ui;
+        package = pkgs.unstable.lxd-unwrapped.ui;
       };
     };
   };
