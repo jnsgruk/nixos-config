58e4da54fa31ec1b0e1ec269e203eb4be89f86a3
Jon Seager <jon@sgrs.uk>
2023-07-17 10:58:24 +0100

    refactor: use homepage-dashboard module from unstable

--- host/common/services/homepage.nix
+++ host/common/services/homepage.nix
@@ -1,22 +1,9 @@
-{ pkgs, ... }: {
-  networking.firewall = {
-    allowedTCPPorts = [ 8082 ];
-  };
+{ inputs, pkgs, ... }: {
+  imports = [ "${inputs.nixpkgs-unstable}/nixos/modules/services/misc/homepage-dashboard.nix" ];
 
-  systemd.services.homepage-dashboard = {
+  services.homepage-dashboard = {
     enable = true;
-    description = "A highly customizable homepage (or startpage / application dashboard) with Docker and service API integrations.";
-    environment = {
-      HOMEPAGE_CONFIG_DIR = "/var/lib/homepage-dashboard";
-      PORT = "8082";
-    };
-    serviceConfig = {
-      Type = "simple";
-      DynamicUser = true;
-      StateDirectory = "homepage-dashboard";
-      ExecStart = "${pkgs.homepage}/bin/homepage";
-      Restart = "on-failure";
-    };
-    wantedBy = [ "multi-user.target" ];
+    package = pkgs.unstable.homepage-dashboard;
+    openFirewall = true;
   };
 }
