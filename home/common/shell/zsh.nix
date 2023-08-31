f52264da28a3d2f294c508d4b03358343131eb7d
Jon Seager <jon@sgrs.uk>
2023-08-31 14:53:10 +0100

    feat: introduce new aliases for reloading nix/home-manager

--- home/common/shell/zsh.nix
+++ home/common/shell/zsh.nix
@@ -78,8 +78,9 @@
         speedtest = "curl -s https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py | python -";
 
         cleanup-nix = "sudo nix-collect-garbage -d";
-        reload-nix = "sudo nixos-rebuild switch --flake /home/jon/nixos-config";
-        reload-home = "home-manager switch --flake /home/jon/nixos-config";
+        rln = "sudo nixos-rebuild switch --flake /home/jon/nixos-config";
+        rlh = "home-manager switch --flake /home/jon/nixos-config";
+        rlb = "rln;rlh";
       };
     };
   };
