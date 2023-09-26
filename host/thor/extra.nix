b137b051dbf6116ec7d33e5990ee7da6c60a7245
Jon Seager <jon@sgrs.uk>
2023-09-26 13:32:48 +0100

    feat: disable NFS shares for thor

--- host/thor/extra.nix
+++ host/thor/extra.nix
@@ -6,12 +6,6 @@
     ../common/services/traefik
   ];
 
-  # Firewall port for NFS
-  networking.firewall = {
-    allowedTCPPorts = [ 111 2049 4000 4001 4002 20048 ];
-    allowedUDPPorts = [ 111 2049 4000 4001 4002 20048 ];
-  };
-
   services = {
     cron = {
       enable = true;
@@ -21,15 +15,6 @@
     };
 
     duplicati.enable = true;
-
-    nfs = {
-      server = {
-        enable = true;
-        exports = ''
-          /data/media  100.64.0.0/10 (rw,fsid=0,no_subtree_check)
-        '';
-      };
-    };
 
     syncthing = {
       enable = true;
