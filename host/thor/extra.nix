b770416480f5693c3c9243be0eed37f9af117a7f
Jon Seager <jon@sgrs.uk>
2023-09-26 14:50:47 +0100

    feat: replace duplicati with borgbackup

--- host/thor/extra.nix
+++ host/thor/extra.nix
@@ -1,4 +1,4 @@
-{ lib, ... }: {
+{ lib, config, ... }: {
   imports = [
     ../common/services/files.nix
     ../common/services/homepage.nix
@@ -6,20 +6,55 @@
     ../common/services/traefik
   ];
 
-  services = {
-    cron = {
-      enable = true;
-      systemCronJobs = [
-        "@daily    root    /data/apps/backup.sh > /data/apps/backup.log"
-      ];
+  age.secrets = {
+    borgbase-ssh = {
+      file = ../../secrets/borgbase-ssh.age;
+      owner = "root";
+      group = "root";
+      mode = "400";
     };
-
-    duplicati.enable = true;
+    borgbase-passphrase = {
+      file = ../../secrets/borgbase-passphrase.age;
+      owner = "root";
+      group = "root";
+      mode = "400";
+    };
   };
 
-  systemd.services.duplicati.serviceConfig = {
-    Group = lib.mkForce "users";
-    User = lib.mkForce "jon";
+  services = {
+    # In order to mount the backup to restore files, perform the following:
+    #
+    #    mkdir backup
+    #    sudo borg-job-borgbase mount ike67cye@ike67cye.repo.borgbase.com:repo ./backup
+    # 
+    # Then copy out the files you need using normal Linux commands. Once complete, unmount
+    # with:
+    #
+    #    borg-job-borgbase umount backup
+    borgbackup.jobs."borgbase" = {
+      paths = [
+        "/data"
+        "/var/lib/homepage-dashboard"
+        "/var/lib/radarr"
+        "/var/lib/sonarr"
+        "/var/lib/prowlarr"
+        "/var/lib/jellyfin"
+        "/var/lib/sanzbd"
+      ];
+      exclude = [
+        "/data/apps/files/_files/cache"
+        "/data/lost+found"
+        "/data/media"
+      ];
+      repo = "ike67cye@ike67cye.repo.borgbase.com:repo";
+      encryption = {
+        mode = "repokey-blake2";
+        passCommand = "cat ${config.age.secrets."borgbase-passphrase".path}";
+      };
+      environment.BORG_RSH = "ssh -i ${config.age.secrets."borgbase-ssh".path}";
+      compression = "auto,lzma";
+      startAt = "daily";
+    };
   };
 }
 
