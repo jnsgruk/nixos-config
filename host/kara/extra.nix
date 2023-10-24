17ed8909c1028923145755b680b72be71f735eeb
Jon Seager <jon@sgrs.uk>
2023-10-24 10:40:58 +0100

    feat: enable hourly borgbase backups for kara

---
+++ host/kara/extra.nix
@@ -0,0 +1,50 @@
+{ config, ... }: {
+  age.secrets = {
+    borgbase-ssh = {
+      file = ../../secrets/kara-borgbase-ssh.age;
+      owner = "root";
+      group = "root";
+      mode = "400";
+    };
+    borgbase-passphrase = {
+      file = ../../secrets/kara-borgbase-passphrase.age;
+      owner = "root";
+      group = "root";
+      mode = "400";
+    };
+  };
+
+  services = {
+    # In order to mount the backup to restore files, perform the following:
+    #
+    #    mkdir backup
+    #    sudo borg-job-borgbase mount z2sqv4mw@z2sqv4mw.repo.borgbase.com:repo ./backup
+    # 
+    # Then copy out the files you need using normal Linux commands. Once complete, unmount
+    # with:
+    #
+    #    borg-job-borgbase umount backup
+    borgbackup.jobs."borgbase" = {
+      paths = [
+        "/home/jon/data"
+      ];
+      exclude = [
+        "**/node_modules"
+        "**/build"
+        "**/dist"
+        "**/.tox"
+        "**/.venv"
+        "**/venv"
+        "**/target"
+      ];
+      repo = "z2sqv4mw@z2sqv4mw.repo.borgbase.com:repo";
+      encryption = {
+        mode = "repokey-blake2";
+        passCommand = "cat ${config.age.secrets."borgbase-passphrase".path}";
+      };
+      environment.BORG_RSH = "ssh -i ${config.age.secrets."borgbase-ssh".path}";
+      compression = "auto,lzma";
+      startAt = "hourly";
+    };
+  };
+}
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,13 @@
 let
   jon = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL3bnlKpGO7eqZFafiLxJVG0TYyleVfuO1C9Q2q0QHJg";
+  kara = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFhVkbNk0hYPZOu4SlBwD1RXd78PmiD21Yen2j8JgT6Z";
   thor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJqnwVNoWVkTJxUJ1yZh64wKBYaij1IwXUA3PRE3/lIx";
   users = [ jon ];
 in
 {
+  "kara-borgbase-ssh.age".publicKeys = users ++ [ kara ];
+  "kara-borgbase-passphrase.age".publicKeys = users ++ [ kara ];
+
   "thor-digitalocean.age".publicKeys = users ++ [ thor ];
   "thor-borgbase-ssh.age".publicKeys = users ++ [ thor ];
   "thor-borgbase-passphrase.age".publicKeys = users ++ [ thor ];
