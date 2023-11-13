39d779a4ad4c421f1e4412a59899555232370fb9
Jon Seager <jon@sgrs.uk>
2023-11-13 15:52:09 +0000

    feat: disable multipass

--- host/common/virt/default.nix
+++ host/common/virt/default.nix
@@ -3,7 +3,7 @@
     ./embr.nix
     ./docker.nix
     ./lxd.nix
-    ./multipass.nix
+    # ./multipass.nix
   ] # Include quickemu if a desktop is defined
   ++ lib.optional (builtins.isString desktop) ./quickemu.nix;
 }
