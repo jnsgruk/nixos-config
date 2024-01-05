2e2298cb037f005f56ba865b664cd7da6173519d
Jon Seager <jon@sgrs.uk>
2024-01-05 10:56:07 +0000

    chore: remove channel override for homepage-dashboard module

--- host/common/services/homepage.nix
+++ host/common/services/homepage.nix
@@ -1,7 +1,4 @@
 { inputs, pkgs, ... }: {
-  disabledModules = [ "${inputs.nixpkgs}/nixos/modules/services/misc/homepage-dashboard.nix" ];
-  imports = [ "${inputs.unstable}/nixos/modules/services/misc/homepage-dashboard.nix" ];
-
   services.homepage-dashboard = {
     enable = true;
     package = pkgs.unstable.homepage-dashboard;
