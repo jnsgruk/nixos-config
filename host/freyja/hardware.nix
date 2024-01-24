5529788cf5bd9cd9e55948707da2cd2f6f14b6a3
Jon Seager <jon@sgrs.uk>
2024-01-24 14:39:36 +0000

    refactor: update fwupd config for freyja, refactor disks

--- host/freyja/hardware.nix
+++ host/freyja/hardware.nix
@@ -4,45 +4,48 @@
   imports = [
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-z13
     ../common/hardware/bluetooth.nix
-    ../common/services/fwupd.nix
   ];
+
+  services.fwupd.enable = true;
 
   nixpkgs.hostPlatform = "x86_64-linux";
 
   # TODO: Replace with Disko config
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
-    fsType = "btrfs";
-    options = [ "subvol=@" ];
-  };
+  filesystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
-    fsType = "btrfs";
-    options = [ "subvol=@home" ];
-  };
+    "/home" = {
+      device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
+      fsType = "btrfs";
+      options = [ "subvol=@home" ];
+    };
 
-  fileSystems."/var" = {
-    device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
-    fsType = "btrfs";
-    options = [ "subvol=@var" ];
-  };
+    "/var" = {
+      device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
+      fsType = "btrfs";
+      options = [ "subvol=@var" ];
+    };
 
-  fileSystems."/.snapshots" = {
-    device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
-    fsType = "btrfs";
-    options = [ "subvol=@snapshots" ];
-  };
+    "/.snapshots" = {
+      device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
+      fsType = "btrfs";
+      options = [ "subvol=@snapshots" ];
+    };
 
-  fileSystems."/.swap" = {
-    device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
-    fsType = "btrfs";
-    options = [ "subvol=@swap" ];
-  };
+    "/.swap" = {
+      device = "/dev/disk/by-uuid/99db9e90-c813-4469-a6a7-bae6b8f49955";
+      fsType = "btrfs";
+      options = [ "subvol=@swap" ];
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/21B1-1362";
-    fsType = "vfat";
+    "/boot" = {
+      device = "/dev/disk/by-uuid/21B1-1362";
+      fsType = "vfat";
+    };
   };
 
   swapDevices = [{
