ad60b1784d9ff20d36ea286c58afb7806f933d30
Jon Seager <jon@sgrs.uk>
2024-02-08 09:50:17 +0000

    fix: ensure that libGL is in the library path for Obsidian

--- overlays/default.nix
+++ overlays/default.nix
@@ -18,6 +18,29 @@
         vendorHash = "sha256-Q6dlb6+mBRx8ZveFvFIXgAGHerzExi9HaSuJKVt1Ogc=";
       });
     };
+
+    # https://github.com/NixOS/nixpkgs/pull/282595
+    # https://github.com/NixOS/nixpkgs/pull/285883
+    # https://github.com/NixOS/nixpkgs/issues/282749
+    obsidian = prev.obsidian.overrideAttrs (oldAttrs: rec {
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/bin
+        makeWrapper ${prev.electron_25}/bin/electron $out/bin/obsidian \
+          --set LD_LIBRARY_PATH "${prev.lib.makeLibraryPath [ prev.libGL ]}" \
+          --add-flags $out/share/obsidian/app.asar \
+          --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform=wayland}}"
+        install -m 444 -D resources/app.asar $out/share/obsidian/app.asar
+        install -m 444 -D resources/obsidian.asar $out/share/obsidian/obsidian.asar
+        install -m 444 -D "${oldAttrs.desktopItem}/share/applications/"* \
+          -t $out/share/applications/
+        for size in 16 24 32 48 64 128 256 512; do
+          mkdir -p $out/share/icons/hicolor/"$size"x"$size"/apps
+          convert -background none -resize "$size"x"$size" ${oldAttrs.icon} $out/share/icons/hicolor/"$size"x"$size"/apps/obsidian.png
+        done
+        runHook postInstall
+      '';
+    });
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
