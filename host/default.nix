737704fdc2e5769e9a6283e210d7aaf4b081b3e0
Jon Seager <jon@sgrs.uk>
2024-03-05 20:37:39 +0000

    fix: correctly map flake inputs -> channels

--- host/default.nix
+++ host/default.nix
@@ -45,6 +45,14 @@
       options = "--delete-older-than 14d";
     };
 
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    registry = lib.mkForce (lib.mapAttrs (_: value: { flake = value; }) inputs);
+
+    # This will additionally add your inputs to the system's legacy channels
+    # Making legacy nix commands consistent as well, awesome!
+    nixPath = lib.mkForce (lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry);
+
     optimise.automatic = true;
     settings = {
       auto-optimise-store = true;
