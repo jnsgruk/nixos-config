ab46f2b45aea11634c85c2c2024eac1c4f5601e0
Jon Seager <jon@sgrs.uk>
2024-03-05 16:00:15 +0000

    chore: update flake lock file

--- host/common/hardware/audioengine.nix
+++ host/common/hardware/audioengine.nix
@@ -1,6 +1,6 @@
-_: {
-  environment.etc = {
-    "wireplumber/main.lua.d/51-disable-suspension.lua".text = ''
+{ pkgs, lib, ... }: {
+  services.pipewire.wireplumber.configPackages = [
+    (pkgs.writeTextDir "share/wireplumber/main.lua.d/51-disable-suspension.lua" ''
       table.insert (alsa_monitor.rules, {
         matches = {
           {
@@ -11,6 +11,6 @@ _: {
           ["session.suspend-timeout-seconds"] = 0,
         },
       })
-    '';
-  };
+    '')
+  ];
 }
