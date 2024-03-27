f6ab9daf4e808083d644116a472f633e1d4ab56f
Jon Seager <jon@sgrs.uk>
2024-03-27 16:03:01 +0000

    conf: switch kara to displayport for monitors

--- home/common/desktop/hyprland/config/displays.nix
+++ home/common/desktop/hyprland/config/displays.nix
@@ -2,18 +2,18 @@ _: {
   kara =
     {
       workspace = [
-        "1, monitor: HDMI-A-1"
-        "2, monitor: HDMI-A-1"
-        "3, monitor: HDMI-A-2"
-        "4, monitor: HDMI-A-2"
-        "5, monitor: HDMI-A-1"
-        "6, monitor: HDMI-A-1"
-        "7, monitor: HDMI-A-1"
+        "1, monitor: DP-2"
+        "2, monitor: DP-2"
+        "3, monitor: DP-1"
+        "4, monitor: DP-1"
+        "5, monitor: DP-2"
+        "6, monitor: DP-2"
+        "7, monitor: DP-2"
       ];
 
       monitor = [
-        "HDMI-A-1, preferred, 0x0, 1"
-        "HDMI-A-2, preferred, 3840x0, 1"
+        "DP-2, preferred, 3840x0, 1"
+        "DP-1, preferred, 0x0, 1"
       ];
     };
 
