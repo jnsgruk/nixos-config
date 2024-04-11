083735c5c0dc86ffe138560943117b6a1447b8ad
Jon Seager <jon@sgrs.uk>
2024-04-11 08:32:45 +0100

    traefik: 3.0.0-rc1 -> 3.0.0-rc4

--- overlays/default.nix
+++ overlays/default.nix
@@ -20,18 +20,18 @@
           _final: prev: {
             traefik = prev.callPackage "${prev.path}/pkgs/servers/traefik" {
               buildGoModule = args: prev.buildGo122Module (args // rec {
-                version = "3.0.0-rc1";
+                version = "3.0.0-rc4";
                 src = prev.fetchzip {
                   url = "https://github.com/traefik/traefik/releases/download/v${version}/traefik-v${version}.src.tar.gz";
-                  sha256 = "sha256-IQOABrwA+39WFnjmWkzl7PFwsXZrQIv8lBu+yZWSlvg=";
+                  sha256 = "sha256-kl4VmyWMRCjAZneHDYLTJ2/xF0BBO+yM/2gia+6R3sQ=";
                   stripRoot = false;
                 };
-                vendorHash = "sha256-CfS3AWNz69U1J9yDwxXu2jlpvit/JdKPqn9fqo3o6W8=";
+                vendorHash = "sha256-Lv4TuH0jB8wOupjCNG+y6APxZqho4AgObHuO4nFpjaE=";
 
                 preBuild = ''
                   GOOS= GOARCH= CGO_ENABLED=0 go generate
 
-                  CODENAME=$(grep -Po "CODENAME := \K.+$" Makefile)
+                  CODENAME=$(grep -Po "CODENAME \?=\s\K.+$" Makefile)
 
                   buildFlagsArray+=("-ldflags= -s -w \
                     -X github.com/traefik/traefik/v${prev.lib.versions.major version}/pkg/version.Version=${version} \
