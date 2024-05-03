dc76a7f86854a1dc4bdc9ccb5d463e709683918c
Jon Seager <jon@sgrs.uk>
2024-05-03 09:10:28 +0100

    chore: remove traefik 3 overlay, now in unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -15,37 +15,7 @@
     unstable = import inputs.unstable {
       inherit (final) system;
       config.allowUnfree = true;
-
-      overlays = [
-        (_final: prev: {
-          traefik = prev.callPackage "${prev.path}/pkgs/servers/traefik" {
-            buildGoModule =
-              args:
-              prev.buildGo122Module (
-                args
-                // rec {
-                  version = "3.0.0-rc5";
-                  src = prev.fetchzip {
-                    url = "https://github.com/traefik/traefik/releases/download/v${version}/traefik-v${version}.src.tar.gz";
-                    sha256 = "sha256-IVnZtperYT1uHhkZmREIoMW2YwHv82rxP8Oy4g8ZF7o=";
-                    stripRoot = false;
-                  };
-                  vendorHash = "sha256-Lv4TuH0jB8wOupjCNG+y6APxZqho4AgObHuO4nFpjaE=";
-
-                  preBuild = ''
-                    GOOS= GOARCH= CGO_ENABLED=0 go generate
-
-                    CODENAME=$(grep -Po "CODENAME \?=\s\K.+$" Makefile)
-
-                    buildFlagsArray+=("-ldflags= -s -w \
-                      -X github.com/traefik/traefik/v${prev.lib.versions.major version}/pkg/version.Version=${version} \
-                      -X github.com/traefik/traefik/v${prev.lib.versions.major version}/pkg/version.Codename=$CODENAME")
-                  '';
-                }
-              );
-          };
-        })
-      ];
+      overlays = [ (_final: prev: { }) ];
     };
   };
 }
