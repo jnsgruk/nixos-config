c1f1e46f865b1a8b0f5702f671cf533fcad29f9e
Jon Seager <jon@sgrs.uk>
2024-05-09 12:53:39 +0100

    fix: start mullvad-daemon without verbose flag

--- host/common/services/mullvad.nix
+++ host/common/services/mullvad.nix
@@ -1,1 +1,8 @@
-_: { services.mullvad-vpn.enable = true; }
+{ pkgs, lib, ... }:
+{
+  services.mullvad-vpn.enable = true;
+
+  # By default the module enables verbose logging by default,
+  # which is quite noisy. This undoes that.
+  systemd.services.mullvad-daemon.serviceConfig.ExecStart = lib.mkForce "${pkgs.mullvad}/bin/mullvad-daemon --disable-stdout-timestamps";
+}
