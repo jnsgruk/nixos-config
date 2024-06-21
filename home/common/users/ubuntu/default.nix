796dbce1520559b096f5fdfc92274ada0c539315
Jon Seager <jon@sgrs.uk>
2024-06-21 12:09:36 +0100

    fix: fix path to base.nix in dev directory for home-manager

--- home/common/users/ubuntu/default.nix
+++ home/common/users/ubuntu/default.nix
@@ -3,6 +3,6 @@ let
   inherit ((import "${self}/host/common/base/packages.nix" { inherit pkgs; })) basePackages;
 in
 {
-  imports = [ "${self}/dev/base.nix" ];
+  imports = [ "${self}/home/common/dev/base.nix" ];
   home.packages = basePackages;
 }
