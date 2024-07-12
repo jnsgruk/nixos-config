a3b0909dc794f93a0fe93da0b2aee1dac29f2ca1
Jon Seager <jon@sgrs.uk>
2024-07-12 20:35:15 +0200

    feat: enable libheif for nautilus previews

--- host/common/desktop/hyprland.nix
+++ host/common/desktop/hyprland.nix
@@ -30,10 +30,16 @@ in
     variables.NIXOS_OZONE_WL = "1";
 
     systemPackages = with pkgs; [
-      polkit_gnome
       gnome.nautilus
       gnome.zenity
+      # Enable HEIC image previews in Nautilus
+      libheif
+      libheif.out
+      polkit_gnome
     ];
+
+    # Enable HEIC image previews in Nautilus
+    pathsToLink = [ "share/thumbnailers" ];
   };
 
   services = {
