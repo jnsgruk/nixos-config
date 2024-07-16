fd4becc16b42fb3a99c0327ddd1f4beef77f0346
Jon Seager <jon@sgrs.uk>
2024-07-16 14:43:37 +0100

    feat: add overlay for juju 4 client

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,10 +3,36 @@
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
-  modifications = _final: _prev: {
+  modifications = _final: prev: {
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+
+    juju = prev.buildGo122Module {
+      pname = "juju";
+      version = "4.0-beta";
+
+      src = prev.fetchFromGitHub {
+        owner = "juju";
+        repo = "juju";
+        rev = "dfee8e90c86bcfb05ffe187175e3dde7b8dcb599";
+        hash = "sha256-UhBq04T/vjxHth1o8ywnltghUtmvZ3uJd/9ju9Oo0rg=";
+      };
+
+      vendorHash = "sha256-7wEHPICUCGAQYqHcB7Y/n8pojGoPLzNSZAuh7DqR38Y=";
+
+      subPackages = [ "cmd/juju" ];
+
+      nativeBuildInputs = [ prev.installShellFiles ];
+
+      doCheck = false;
+
+      postInstall = ''
+        for file in etc/bash_completion.d/*; do
+          installShellCompletion --bash "$file"
+        done
+      '';
+    };
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
