ba1fa465e555d59f4875043297cc19af164127ce
Jon Seager <jon@sgrs.uk>
2024-09-01 16:53:27 +0100

    feat: use hyprland cachix repo

--- host/default.nix
+++ host/default.nix
@@ -67,6 +67,8 @@
         "nix-command"
         "flakes"
       ];
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
       trusted-users = [
         "root"
         "@wheel"
