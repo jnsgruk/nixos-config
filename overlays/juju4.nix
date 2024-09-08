a54cf6876fe9efcff0704d4cf868ff32876caf9a
Jon Seager <jon@sgrs.uk>
2024-09-08 16:23:16 +0100

    juju4: 4.0-unstable-2024-08-08 -> 4.0-unstable-2024-09-08

--- overlays/juju4.nix
+++ overlays/juju4.nix
@@ -1,19 +1,19 @@
 { pkgs }:
 let
-  inherit (pkgs) buildGo122Module fetchFromGitHub installShellFiles;
+  inherit (pkgs) buildGo123Module fetchFromGitHub installShellFiles;
 in
-buildGo122Module {
+buildGo123Module {
   pname = "juju4";
-  version = "4.0-unstable-2024-08-08";
+  version = "4.0-unstable-2024-09-08";
 
   src = fetchFromGitHub {
     owner = "juju";
     repo = "juju";
-    rev = "e4724547ea17482dea30541463973600e172d878";
-    hash = "sha256-HQ7QPR3bfHKog5b7qw6rsWZyrwvfL/KRSzHSi+Lh+14=";
+    rev = "e51606a12490923092eb8f99882b2c4de7f4af7a";
+    hash = "sha256-UZUQqlUzsjf52BJ8lGMWqUM+pKDHplMZLlcHFQY4c1c=";
   };
 
-  vendorHash = "sha256-zRGxS2QFukI7AKgC8XHon/iH6bzrEsGjFPFf8CLJp4o=";
+  vendorHash = "sha256-KzR7JCjmfGe5nNuXAk4SsxGdpXjzV1+Dc7YOQc1TwcQ=";
 
   subPackages = [ "cmd/juju" ];
 
