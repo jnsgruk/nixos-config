eb4a6278ea35d2726ecaf7990502b3cba7f00c23
Jon Seager <jon@sgrs.uk>
2024-12-02 16:08:58 +0000

    chore: remove unused `Cider` desktop entry

--- home/common/desktop/xdg.nix
+++ home/common/desktop/xdg.nix
@@ -9,21 +9,5 @@ in
       enable = true;
       defaultApplications = associations;
     };
-
-    desktopEntries = {
-      # This is obviously brittle, but works around some difficulties in the (current)
-      # distribution model of the Cider beta, where they drop binaries onto itch.io.
-      cider = {
-        name = "Cider";
-        exec = "${pkgs.appimage-run}/bin/appimage-run -- /home/jon/data/apps/Cider.AppImage";
-        terminal = false;
-        icon = "cider";
-        type = "Application";
-        categories = [
-          "Audio"
-          "Application"
-        ];
-      };
-    };
   };
 }
